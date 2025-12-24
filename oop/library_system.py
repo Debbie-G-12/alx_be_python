f9b91fceb430d255e0d1736fded3e8656eb58d21
Deborah <yhwh.dgse@gmail.com>
2025-12-24 19:47:04 +0300

    Add Bank class and book management functionality

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,3 +1,37 @@
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = []
+
+    def add_account(self, account):
+        self.accounts.append(account)
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
     def list_books(self):
         for book in self.books:
             if isinstance(book, EBook):
